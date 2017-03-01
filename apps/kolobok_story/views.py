90bfa157ebbfdf6158bb7369e5a0dc41d531ca11
Assuliya <t.nesterchuk@mail.ru>
2017-02-28 20:57:47 -0800

    test at the end of the story is working

--- apps/kolobok_story/views.py
+++ apps/kolobok_story/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib import messages
 
 def index(request):
-    return render(request, 'kolobok_story/index.html')
+    if request.method == "GET":
+        return render(request, 'kolobok_story/index.html')
+    if request.method == "POST":
+        answers = ['d','a','d','b','b','c']
+        result = []
+        count = 0
+        for idx, val in enumerate(answers):
+            question_num = idx + 1
+            if request.POST[str(question_num)] == val:
+                dic = {'number':question_num, 'check': 'Right'}
+                count += 1
+            else:
+                dic = {'number':question_num, 'check': 'Wrong'}
+            result.append(dic)
+        context = {'result':result, 'count':count}
+        return render(request, 'kolobok_story/index.html', context)
